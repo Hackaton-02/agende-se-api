51bb9070f3b186bf4c6cd381137c8f4b55cdfabf
João Otávio <joaotavioos@outlook.com>
2022-04-13 23:00:05 -0300

    feat: adding user model initial configuration ✨

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
- 
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_one :address
+  validates :name, presence: true
+  validates :profile, presence: true
+  enum profile: { admin: 0, paciente: 1, especialista: 2 }
+
 end
