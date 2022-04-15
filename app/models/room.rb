2fbfb443f214730e713f38b08908afe42966b1e3
João Otávio <joaotavioos@outlook.com>
2022-04-15 13:21:50 -0300

    feat: adding room model ✨

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,8 @@
 class Room < ApplicationRecord
+    has_one :room_feature
+    
     validates :name, presence: true, uniqueness: { case_sensitive: false }
-    validates :price, presence: true
-    validates :description, presence: true
+    validates :price, presence: true, numericality: { greater_than: 0 }
+    validates :description, :avaliable, presence: true
 
 end
