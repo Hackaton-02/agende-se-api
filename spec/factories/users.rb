59715e0eb97770a0d44a505a8d678f35c92e56d2
João Otávio <joaotavioos@outlook.com>
2022-04-15 12:45:40 -0300

    feat: adding user admin tests ✨

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,7 @@ FactoryBot.define do
       password { "123456" }
       password_confirmation { "123456" }
       profile { :admin }
+      whatsapp_avaliable { Faker::Boolean.boolean }
+      phone { Faker::PhoneNumber.cell_phone_in_e164 }
     end
   end
