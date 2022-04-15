46440a11977065eadd2462b7c64b9cb925db5fca
João Otávio <joaotavioos@outlook.com>
2022-04-15 12:50:32 -0300

    test: adding payment tests 🚨

---
+++ spec/models/consult_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe Consult, type: :model do
+  it { is_expected.to validate_presence_of(:started_at) }
+  it { is_expected.to validate_presence_of(:finish_at) }
+  it { is_expected.to belong_to :user }
+
+  it "can't have past started_at" do
+    subject.started_at = 1.day.ago
+    subject.valid?
+    expect(subject.errors.keys).to include :started_at
+  end
+
+  it "is invalid with current due_date" do
+    subject.started_at = Time.zone.now
+    subject.valid?
+    expect(subject.errors.keys).to include :started_at
+  end
+
+  it "is valid with future date" do
+    subject.started_at = Time.zone.now + 1.hour
+    subject.valid?
+    expect(subject.errors.keys).to_not include :started_at
+  end
+
+  it "can't have past finish_at" do
+    subject.finish_at = 1.day.ago
+    subject.valid?
+    expect(subject.errors.keys).to include :finish_at
+  end
+
+  it "is invalid with current finish_at" do
+    subject.finish_at = Time.zone.now
+    subject.valid?
+    expect(subject.errors.keys).to include :finish_at
+  end
+
+  it "is valid with future date" do
+    subject.finish_at = Time.zone.now + 1.hour
+    subject.valid?
+    expect(subject.errors.keys).to_not include :finish_at
+  end
+end
