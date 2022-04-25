9524f2edda4fb13264534241a1d2478150efccc9
João Otávio <joaotavioos@outlook.com>
2022-04-25 20:06:39 -0300

    feat: add has_many room_rent association ✨

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,8 @@ class Room < ApplicationRecord
     include LikeSearchable
     include Paginatable
 
+    has_many :room_rents, dependent: :destroy
+
     validates :name, presence: true, uniqueness: { case_sensitive: false }
     validates :price, presence: true, numericality: { greater_than: 0 }
     validates :description, presence: true
