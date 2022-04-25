82bad2cc265e34858f4dd4ed18173d2ceeb889dd
João Otávio <joaotavioos@outlook.com>
2022-04-25 00:46:56 -0300

    test: adding models tests 🚨

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,9 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:phone) }
   it { is_expected.to allow_value(%w(true false)).for(:whatsapp_avaliable) }
   it { is_expected.not_to allow_value(nil).for(:whatsapp_avaliable) }
+  it { is_expected.to have_many(:room_rents).dependent(:destroy) }
+  it { is_expected.to have_many(:consults).dependent(:destroy) }
+  it { is_expected.to have_one(:address).dependent(:destroy) }
 
   it_has_behavior_of "like searchable concern", :user, :name
   it_behaves_like "paginatable concern", :user
