68efcfc796fcc6703d1a957c97772634b7096aec
João Otávio <joaotavioos@outlook.com>
2022-05-04 09:11:40 -0300

    refactor: code refactoring 📦

--- spec/factories/rooms.rb
+++ spec/factories/rooms.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :room do
     sequence(:name) { |n| "Room #{n}" }
     description { Faker::Lorem.paragraph(sentence_count: 35) }
-    price { Faker::Commerce.price(range: 20.0..1000.0) }
+    price { Faker::Commerce.price(range: 800.0..10000.0) }
     avaliable { true }
   end
 end
