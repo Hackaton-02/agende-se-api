774d3822c59a6197d542cf172aed9d8cce96d10b
João Otávio <joaotavioos@outlook.com>
2022-05-04 09:05:16 -0300

    test: update consult validation controller tests 🚨

--- spec/requests/storefront/v1/consults_validations/consults_validations_request_spec.rb
+++ spec/requests/storefront/v1/consults_validations/consults_validations_request_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe "Storefront::V1::ConsultsValidations", type: :request do
     let!(:login_user) { create(:user) }
     let!(:room_rent) { create(:room_rent) }
-    let!(:consult) { create(:consult, room_rent_id: room_rent.id)}
+
     let(:url) { "/storefront/v1/consults/#{room_rent.id}/validations" }
     
+    context "with one consult" do
+      let!(:consult) { create(:consult, room_rent_id: room_rent.id)}
 
     it 'returns booked consults' do
      get url, headers: auth_header(login_user)
@@ -22,13 +24,40 @@ RSpec.describe "Storefront::V1::ConsultsValidations", type: :request do
      get url, headers: auth_header(login_user)
      expect(response).to have_http_status(:ok)
     end 
+  end
+    context "with more than one consult" do
+    
+      let!(:consult) { create(:consult, room_rent_id: room_rent.id)}
+      let!(:consult2) { create(:consult, room_rent_id: room_rent.id)}
+   
+         it 'returns all Consults' do
+           get url, headers: auth_header(login_user)
+           expected_return = build_array(Consult.all)
+           expect(body_json).to eq expected_return
+         end
+         
+         it "returns all dates" do
+           get url, headers: auth_header(login_user)
+           expect(body_json.count).to eq 1
+         end
+         
+         it "returns success status" do
+           get url, headers: auth_header(login_user)
+           expect(response).to have_http_status(:ok)
+         end 
+      end
 
   def build(rent)
-    array = []
-    json = rent.as_json(only: %i[range]).to_h
-    json.to_a
+    json = rent.as_json(only: %i[range])
     json['range'] = ((rent.started_at.to_date..rent.finish_at.to_date).to_a).as_json()
-    array.push(json)
-    array
+    json
+  end
+
+  def build_array(array)
+    json = {}
+    range = []
+    array.map { |data| range.push((data.started_at.to_date..data.finish_at.to_date).to_a).as_json }
+    json['range'] = range.flatten.as_json
+    json
   end
 end
